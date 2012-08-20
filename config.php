99ff20f3101529233464b485f2e11b42a93b2b34
Morten Nilsen <morten@runsafe.no>
2012-08-20 22:13:45 +0200

    Move configuration into a separate file

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+	$admin = '/var/www/data/gitolite-admin';
+	$config = $admin.'/conf/gitolite.conf';
+	$keys = $admin.'/keydir';
+?>
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-	$admin = '/var/www/data/gitolite-admin';
-	$config = $admin.'/conf/gitolite.conf';
-	$keys = $admin.'/keydir';
+	require_once('config.php');
 
 	chdir($admin);
 	exec('git pull');
